b78842c6b79f334ed9c40efcb2768f2c8dbb879d
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-04-10 20:47:26 -0300

    Ícone para mobile e desktop

---
+++ src/components/Icons/logo.tsx
@@ -0,0 +1,27 @@
+import { FunctionComponent } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { ReactComponent as Quezze } from "../../assets/svg/logo.svg";
+import { ReactComponent as QuezzeDesktop } from "../../assets/svg/logo_desktop.svg";
+import { iconConfig } from "./../../config/index";
+import { LogoDiv } from "./style";
+
+export const Logo: FunctionComponent<LogoParams> = ({ onClick }) => {
+  return (
+    <LogoDiv onClick={onClick}>
+      <AiOutlineMenu {...iconConfig} />
+      <Quezze />
+    </LogoDiv>
+  );
+};
+
+export const LogoDesktop: FunctionComponent<LogoParams> = ({ onClick }) => {
+  return (
+    <LogoDiv onClick={onClick}>
+      <QuezzeDesktop />
+    </LogoDiv>
+  );
+};
+
+export interface LogoParams {
+  onClick?: (param: any) => void;
+}
---
+++ src/components/Icons/style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components/macro";
+import { Div } from "../../assets/styled/base";
+
+export const LogoDiv = styled(Div)`
+  width: auto;
+`;
