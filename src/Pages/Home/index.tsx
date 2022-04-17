436d3aff939bf3ba59c25949b5ecf76fb428e3d9
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-04-16 21:18:27 -0300

    Adicionando swiper na home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,5 +2,11 @@ import { memo } from "react";
 import QuizSwiper from "../../components/swiper";
 
 export default memo(function Home() {
-  return <QuizSwiper />;
+  return (
+    <>
+      <QuizSwiper />
+      <QuizSwiper />
+      <QuizSwiper />
+    </>
+  );
 });
