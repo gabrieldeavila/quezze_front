9ebcb70ed444c30ca613917c8e7c227368ce5156
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-05-12 21:40:33 -0300

    Refatorando BasicInfo para retornar ação a ser chamada após onChange

--- src/Pages/Create/BasicInfo.tsx
+++ src/Pages/Create/BasicInfo.tsx
@@ -7,27 +7,34 @@ import { Button } from "./../../assets/styled/base";
 import { translateSelect } from "../../components/Form/Fields/select/translateSelect";
 import { ex } from "./data_example";
 import { CreateInterface } from "./../../redux/interfaces/Create";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setCreate } from "../../redux/effects/Create";
 import { useTranslation } from "react-i18next";
 import { BasicInfoProps } from "./interface";
+import { AppState } from "../../redux/store";
 
 function BasicInfo({ setShowQuestions }: BasicInfoProps) {
   const options = translateSelect({ options: ex }, "create.types");
-  const dispatch = useDispatch();
+  const create: CreateInterface = useSelector(
+    (state: AppState) => state.create
+  );
 
   const { t } = useTranslation();
 
   // prettier-ignore
   const handleSubmit = useCallback((values: CreateInterface) => {
     setShowQuestions(true);
-    dispatch(setCreate(values));
-  },[dispatch]);
+  },[]);
 
   return (
     <>
       <H1Light>{t("create.title")}</H1Light>
-      <Form schema="create" onSubmit={handleSubmit}>
+      <Form
+        action={setCreate}
+        initialValues={create}
+        schema="create"
+        onSubmit={handleSubmit}
+      >
         <input type="text" name="name" />
         <Select
           options={options}
--- src/Pages/Create/index.tsx
+++ src/Pages/Create/index.tsx
@@ -2,18 +2,16 @@ import { useSelector } from "react-redux";
 import { AppState } from "../../redux/store";
 import BasicInfo from "./BasicInfo";
 import { useEffect, useState } from "react";
-import { CreateProps } from "./../../redux/interfaces/Create";
+import { CreateInterface } from "./../../redux/interfaces/Create";
 import _ from "lodash";
-import QuestionCreator from "./QuestionCreator";
+import QuestionsCreator from "../../components/Questions";
 
 export default function Create() {
-  const posts: CreateProps = useSelector((state: AppState) => state.create);
-
   const [showQuestions, setShowQuestions] = useState(false);
 
   return !showQuestions ? (
     <BasicInfo setShowQuestions={setShowQuestions} />
   ) : (
-    <QuestionCreator />
+    <QuestionsCreator />
   );
 }
