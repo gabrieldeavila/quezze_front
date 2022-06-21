73e6e360008fb48cdc77f02c641266320e1545a6
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-06-20 21:34:21 -0300

    Adicionando imagem para tela de quizze

---
+++ src/Pages/Play/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { flex } from "./../../assets/styled/mixins";
+
+export const PlayImg = styled.img`
+  border-radius: 5px;
+  width: 50rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+`;
+
+export const PlayWrapper = styled.div`
+  ${flex}
+  flex-direction: column;
+  gap: 1rem;
+`;
